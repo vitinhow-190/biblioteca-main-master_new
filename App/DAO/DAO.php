739177e08cf713668b8b174574497e257dee6929
231471 <231471@CPD02-M07>
2025-08-04 15:58:27 -0300

    04/08

--- App/DAO/DAO.php
+++ App/DAO/DAO.php
@@ -21,7 +21,8 @@ abstract class DAO extends PDO
                 $_ENV['db']['user'],
                 $_ENV['db']['pass'],
                 [
-                    PDO::ATTR_PERSISTENT => true,                    
+                    PDO::ATTR_PERSISTENT => TRUE,
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXEPTION,                    
                     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4'
                 ]
             ); // Fecha construtor da classe PDO 
